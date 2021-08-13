25fa00f6660bcf1b63c47ea79a46083e9e09d69e
金钊 <jinzhao@csii.com.cn>
2021-08-13 13:49:39 +0800

    spring docker 1

---
+++ cloud-client/src/main/java/com/client/cloudclient/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.client.cloudclient.controller;
+
+import com.client.cloudclient.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class UserController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return restTemplate.getForObject("http://localhost:8085/" + id, User.class);
+    }
+}
