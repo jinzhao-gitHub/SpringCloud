2ca547a0e7c339c74e596201dbd5e7bb3e7872ea
金钊 <jinzhao@csii.com.cn>
2021-08-13 13:50:18 +0800

    spring docker 1

--- cloud-client/src/main/java/com/client/cloudclient/CloudClientApplication.java
+++ cloud-client/src/main/java/com/client/cloudclient/CloudClientApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,8 +20,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 public class CloudClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudClientApplication.class, args);
-	}
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudClientApplication.class, args);
+    }
 
 }
---
+++ cloud-provider/src/main/java/com/provider/cloudprovider/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.provider.cloudprovider.controller;
+
+import com.provider.cloudprovider.dao.UserRepository;
+import com.provider.cloudprovider.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id){
+        User findOne = userRepository.findById(id).get();
+        return findOne;
+    }
+}
